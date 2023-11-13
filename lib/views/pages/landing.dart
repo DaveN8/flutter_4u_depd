a4157401cc2360391d4bf1af1c70f0a1293ccdc7
JevonCledwyn <jcledwyn@student.ciputra.ac.id>
2023-11-13 14:56:11 +0700

    Update NavBar
    add logo and 4U title, add image banner asset

--- lib/views/pages/landing.dart
+++ lib/views/pages/landing.dart
@@ -19,105 +19,119 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Landing Page'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // 1. Navbar
-            Container(
-              color: Colors.blue, // Example color for navbar
-              height: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Navbar contents (e.g., logo, menu items)
-                  // Replace with your own navbar elements
-                ],
-              ),
-            ),
-
-            // 2. Banner
-            Container(
-              height: 200,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                      'assets/banner_image.jpg'), // Replace with your image
-                  fit: BoxFit.cover,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.blue, // Example color for navbar
+            expandedHeight: 50.0,
+            pinned: true, // Make the app bar pinned at the top
+            title: Row(
+              children: [
+                Image.asset(
+                  'images/logodei.jpg',
+                  width: 40, // Adjust width as needed
+                  height: 40, // Adjust height as needed
                 ),
-              ),
-              // Additional banner contents (if any)
-            ),
-
-            // 3. Explanation about services using dynamic data
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  Text(
-                    'Our Services',
+                SizedBox(width: 10), // Add spacing between logo and "4U" title
+                GestureDetector(
+                  onTap: () {
+                    // Add the action you want when the "4U" title is tapped,
+                    // for example, navigate to the homepage.
+                    // Navigator.pushNamed(context, '/homepage'); // Example navigation
+                  },
+                  child: Text(
+                    '4U',
                     style: TextStyle(
-                      fontSize: 24,
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
+                      color: Colors.white, // Adjust color as needed
                     ),
                   ),
-                  SizedBox(height: 20),
-                  // Display services dynamically
-                  Row(
-                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    // children: services,
-                    //     .map((services) => _buildServiceSection(
-                    //         services['title']!, services['description']!))
-                    //     .toList(),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-
-            // 4. Products section using dynamic data
-            Container(
-              height: 200, // Adjust height as needed
-              color: Colors.grey, // Example color for product section
-              child: Row(
-                // children: products
-                //     .map((product) => Expanded(child: _buildProduct(product)))
-                //     .toList(),
-              ),
-            ),
-
-            // 5. Form section
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  Text(
-                    'Fill in the Form',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                // 2. Banner
+                Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/banner_image.jpg'), // Replace with your image
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  SizedBox(height: 20),
-                  // Form fields for name, email, and telephone number
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Name'),
+                  // Additional banner contents (if any)
+                ),
+
+                // 3. Explanation about services using dynamic data
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Our Services',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      // Display services dynamically
+                      Row(
+                        children: widget.services
+                            .map((services) => _buildServiceSection(
+                                services['title']!, services['description']!))
+                            .toList(),
+                      ),
+                    ],
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Email'),
+                ),
+
+                // 4. Products section using dynamic data
+                Container(
+                  height: 200, // Adjust height as needed
+                  color: Colors.grey, // Example color for product section
+                  child: Row(
+                    children: widget.products
+                        .map((product) => Expanded(child: _buildProduct(product)))
+                        .toList(),
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Telephone Number'),
+                ),
+
+                // 5. Form section
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Fill in the Form',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      // Form fields for name, email, and telephone number
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Name'),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Email'),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Telephone Number'),
+                      ),
+                      // Additional form submission button or action
+                    ],
                   ),
-                  // Additional form submission button or action
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
